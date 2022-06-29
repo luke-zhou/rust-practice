09732d5bd85f18de6d855752cae155c4182e7bd1
Luke Zhou <sharlock@gmail.com>
2022-06-29 11:13:08 +1000

    implement bubule sort

--- bubble-sort/src/lib.rs
+++ bubble-sort/src/lib.rs
@@ -1,13 +1,19 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
+pub fn bubble_sort(list: & mut Vec<i64>) {
+    for i in 0..list.capacity(){
+        let mut swapped = false;
+        for j in 1..(list.capacity()-i) {
+            if list[j-1] > list[j] {
+                let temp = list[j-1];
+                list[j-1] = list[j];
+                list[j] = temp; 
+                swapped = true;
+            }
+        }
+        if !swapped {
+            break;
+        }
     }
+
 }
 
 #[cfg(test)]
@@ -15,16 +21,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
+    fn bubble_sort_test() {
+        let mut list = vec![10,9,8,7,6,5,4];
+        bubble_sort(& mut list);
+        assert_eq!(list, vec![4, 5, 6, 7, 8, 9, 10]);
     }
+
+    // #[bench]
+    // fn bubble_sort_bench(b: &mut Bencher) {
+    //     let mut list: Vec<i64> = Vec::with_capacity(1_0000);
+    //     for _ in 0..list.capacity() {
+    //         list.push(rand::random());
+    //     };
+    //     bubble_sort(& mut list);
+    //     b.iter(|| bubble_sort(& mut list));
+    // }
 }
